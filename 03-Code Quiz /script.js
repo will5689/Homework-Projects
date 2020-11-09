a2721d8a94f46a3f70873133bc6907c1f018c4e5
will collins <willdoinc@gmail.com>
2020-11-09 12:14:39 -0600

    Smae changes to the labeling files

---
+++ 03-Code Quiz /script.js
@@ -0,0 +1,22 @@
+// Start button hide
+
+
+
+
+var questions1 = {
+  (Q: "What does HTML mean",
+    1: 'Nothing',
+    2: 'lucky guess',
+    3: 'Need another guess')
+};
+
+questions1[0] = "Question"
+document.getElementById('question').innerHTML = questions1[0];
+console.log(questions1);
+
+var ANSWERS =
+  // START HIDE 
+  startButton.addEventListener("on-click", startQuiz);
+
+//NEED TO SHOW THE ANSWERS
+// 
