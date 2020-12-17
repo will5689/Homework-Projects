164562b1a8bab32438bbda2846d54da39b4421ef
will collins <willdoinc@gmail.com>
2020-12-16 21:24:06 -0600

    new

---  04-Code-Quiz /script.js
+++  04-Code-Quiz /script.js
@@ -1,170 +1,145 @@
-const startButton = document.getElementById('start-btn')
-const questionEl = document.getElementById('question')
-const questionContainerEl = document.getElementById('question-container')
-const answerBtnsEl = document.getElementById('answer-btns')
-const timerEl = document.getElementById('timer');
-const setHighScoreEl = document.getElementById("high-scores-list")
+const startButton = document.getElementById("start-btn");
+const questionEl = document.getElementById("question");
+const questionContainerEl = document.getElementById("question-container");
+const answerBtnsEl = document.getElementById("answer-btns");
+const timerEl = document.getElementById("timer");
+const setHighScoreEl = document.getElementById("high-scores-list");
 
-points = 10
-timer = 75
-highScore = 0
-  
+points = 10;
+timer = 75;
+highScore = 0;
 
-let shuffledQuestions, currentQuestionIndex = 0
+let shuffledQuestions,
+  currentQuestionIndex = 0;
 
 startButton.addEventListener("click", startQuiz);
 
-
-
-
 function startQuiz() {
-  console.log("started")
-  startButton.classList.add("hide")
-  timerEl.innerHTML = timer
+  console.log("started");
+  startButton.classList.add("hide");
+  timerEl.innerHTML = timer;
   stopwatch = setInterval(() => {
-
-    timer = timer - 1
-    console.log("Time me", timer)
-    timerEl.innerHTML = timer
-    console.log(currentQuestionIndex, questions.length, "look at me ")
+    timer = timer - 1;
+    //console.log("Time me", timer)
+    timerEl.innerHTML = timer;
+    //console.log(currentQuestionIndex, questions.length, "look at me ")
     if (timer === 0 || currentQuestionIndex === questions.length) {
-      clearInterval(stopwatch)
-      setHighScore()
-      console.log("im the high score", highScore)
+      clearInterval(stopwatch);
+      setHighScore();
+      console.log("im the high score", highScore);
     }
-  }, 1000)
+  }, 1000);
 
-
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerEl.classList.remove("hide")
-  setNextQuestion()
-};
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerEl.classList.remove("hide");
+  setNextQuestion();
+}
 
 function setNextQuestion() {
-  console.log(questions.length, currentQuestionIndex)
+  console.log(questions.length, currentQuestionIndex);
   if (questions.length === currentQuestionIndex) {
-    return setHighScore()
-      
-    
-    console.log(highScore)
-  
+    return setHighScore();
+
+    console.log(highScore);
   }
 
-
-
-
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-  console.log("next question ", shuffledQuestions, currentQuestionIndex)
-  currentQuestionIndex++
-};
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+  console.log("next question ", shuffledQuestions, currentQuestionIndex);
+  //currentQuestionIndex++
+}
 
 function setHighScore() {
-  console.log("its the end of the Game")
-  setHighScoreEl.innerText = points
-  questionContainerEl.classList.add("hide")
-
+  console.log("its the end of the Game");
+  setHighScoreEl.innerText = points;
+  questionContainerEl.classList.add("hide");
 
   if (points > highScore) {
-    highScore = points
+    highScore = points;
   }
 
-  console.log(highScore, "heres a new line ")
-
-  
-};
-
+  console.log(highScore, "heres a new line ");
+}
 
 function showQuestion(question) {
-  console.log('hi');
-  questionEl.innerText = question.question
-  answerBtnsEl.innerHTML = undefined
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn-grid')
+  console.log("hi");
+  questionEl.innerText = question.question;
+  answerBtnsEl.innerHTML = undefined;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn-grid");
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct;
     }
-    button.addEventListener('click', function () {
-      console.log('Something new', answer)
-      selectAnswer(answer)
-
-    })
-    console.log('IM answer', answerBtnsEl);
-    answerBtnsEl.appendChild(button)
-
-
+    button.addEventListener("click", function () {
+      console.log("Something new", answer);
+      selectAnswer(answer);
+    });
+    console.log("IM answer", answerBtnsEl);
+    answerBtnsEl.appendChild(button);
   });
 }
 
-
-
-function resetState(question) {
-
-};
+function resetState(question) {}
 
 function selectAnswer(ans) {
   if (ans.correct === true) {
-    points = points + 10
-    console.log("im adding points")
-    const scoreEl = document.getElementById("score").innerHTML = points;
+    points = points + 10;
+    console.log("im adding points");
+    document.getElementById("score").innerHTML = points;
   } else {
-    timer = timer - 10
+    timer = timer - 10;
   }
 
+  currentQuestionIndex++;
+  setNextQuestion();
+  console.log("Im an a", ans.correct);
+}
 
-  setNextQuestion()
-  console.log("Im an a", ans.correct)
-};
-
-
-
-const questions = [{
+const questions = [
+  {
     question: "What does HTML stand for?",
-    answers: [{
+    answers: [
+      {
         text: "Hyper Text Markup Language",
-        correct: true
+        correct: true,
       },
 
       {
         text: "Hyperlinks and text markup Languages",
-        correct: false
+        correct: false,
       },
       {
         text: "Home tool markup Languages",
-        correct: false
-      }
-
-    ]
+        correct: false,
+      },
+    ],
   },
   {
     question: "What does CSS Stand for?",
-    answers: [{
+    answers: [
+      {
         text: "nothing ",
-        correct: true
+        correct: true,
       },
       {
         text: "Cascading Style Sheet",
-        correct: false
-      }
-    ]
+        correct: false,
+      },
+    ],
   },
   {
     question: " Text for Question",
-    answers: [{
+    answers: [
+      {
         text: "great Idea",
-        correct: true
+        correct: true,
       },
       {
         text: "bad Idea",
-        correct: false
-      }
-    ]
-
-  }
-
+        correct: false,
+      },
+    ],
+  },
 ];
-
-
-
--- 05-Day Planner  /Develop/script.js
+++ 05-Day Planner  /Develop/script.js
@@ -2,116 +2,118 @@
 
 
 
+$('.saveBtn').on('click', function(event){
+    event.preventDefault();
+    var inputEl = $(this).parent().parent().children(".form-control");
+    console.log(inputEl);
+    // const hmmm = document.getElementById("id");
+    // var hours = ["09", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-    const hmmm = document.getElementById("id");
-    var hours = ["09", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    // var dayHours = {
+    //     id: "ID",
+    //     hour: ["10000"],
+    //     time: "TIME"
+    // };
+    // console.log(dayHours.hour);
 
-    var dayHours = {
-        id: "ID",
-        hour: ["10000"],
-        time: "TIME"
-    };
-    console.log(dayHours.hour);
-
-    const hourList = [
-        {
-            id: "0",
-            hour: "09",
-            time: "am",
+    // const hourList = [
+    //     {
+    //         id: "0",
+    //         hour: "09",
+    //         time: "am",
             
-        },
-        {
-            id: "1",
-            hour: "10",
-            time: "am",
+    //     },
+    //     {
+    //         id: "1",
+    //         hour: "10",
+    //         time: "am",
             
 
-        },
-        {
-            id: "2",
-            hour: "11",
-            time: "am",
+    //     },
+    //     {
+    //         id: "2",
+    //         hour: "11",
+    //         time: "am",
             
-        },
-        {
-            Id: "3",
-            hour: "12",
-            time: "am",
+    //     },
+    //     {
+    //         Id: "3",
+    //         hour: "12",
+    //         time: "am",
             
-        },
-        {
-            id: "4",
-            hour: "1",
-            time: "pm",
+    //     },
+    //     {
+    //         id: "4",
+    //         hour: "1",
+    //         time: "pm",
             
-        },
-        {
-            id: "5",
-            hour: "2",
-            time: "pm",
+    //     },
+    //     {
+    //         id: "5",
+    //         hour: "2",
+    //         time: "pm",
             
-        },
-        {
-            id: "6",
-            hour: "3",
-            time: "pm",
+    //     },
+    //     {
+    //         id: "6",
+    //         hour: "3",
+    //         time: "pm",
             
-        },
-        {
-            id: "7",
-            hour: "4",
-            time: "pm",
+    //     },
+    //     {
+    //         id: "7",
+    //         hour: "4",
+    //         time: "pm",
            
-        },
-        {
-            id: "8",
-            hour: "5",
-            time: "pm",
+    //     },
+    //     {
+    //         id: "8",
+    //         hour: "5",
+    //         time: "pm",
             
-        },
-        {
-            id: "9",
-            hour: "6",
-            time: "pm",
+    //     },
+    //     {
+    //         id: "9",
+    //         hour: "6",
+    //         time: "pm",
             
-        },
-        {
-            id: "10",
-            hour: "7",
-            time: "pm",
+    //     },
+    //     {
+    //         id: "10",
+    //         hour: "7",
+    //         time: "pm",
             
-        },
-        {
-            id: "11",
-            hour: "8",
-            time: "pm",
+    //     },
+    //     {
+    //         id: "11",
+    //         hour: "8",
+    //         time: "pm",
             
-        },
-        {
-            id: "12",
-            hour: "9",
-            time: "pm",
+    //     },
+    //     {
+    //         id: "12",
+    //         hour: "9",
+    //         time: "pm",
             
-        },
-    ];
+    //     },
+    // ];
  
 
-    for (var i = 0; i < hourList.length; i++) {
-        console.log("The Time is " + hourList[i]);
-    }
+    // for (var i = 0; i < hourList.length; i++) {
+    //     console.log("The Time is " + hourList[i]);
+    // }
 
        
-    for (var i = 0; i < dayHours; i++) {
-        display.content; newC
+    // for (var i = 0; i < dayHours; i++) {
+    //     display.content; newC
   
-    }
+    // }
 
-    console.log(hourList)
-    window.localStorage.setItem('time', JSON.stringify(dayHours));
+    // console.log(hourList)
+    // window.localStorage.setItem('time', JSON.stringify(dayHours));
 
-    //display  (id, hour, time);
+    // //display  (id, hour, time);
 
 
-    const test = localStorage.getItem('id');
+    // const test = localStorage.getItem('id');
  
-
