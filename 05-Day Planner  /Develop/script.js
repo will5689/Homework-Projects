5f8dae61b57bcfc3a4cc333ee69991d09887f56a
will collins <willdoinc@gmail.com>
2020-12-18 10:15:06 -0600

    Final changes

--- 05-Day Planner  /Develop/script.js
+++ 05-Day Planner  /Develop/script.js
@@ -1,119 +1,19 @@
+// var inputEl = [];
+// var day = moment();
+// const saveBtnEl = document.getElementById("saveBtn");
 
+// $(".saveBtn").on("click", function () {
+//   var inputEl = $(this)("form-control");
+//   console.log(inputEl);
+// });
 
-
-
-$('.saveBtn').on('click', function(event){
-    event.preventDefault();
-    var inputEl = $(this).parent().parent().children(".form-control");
-    console.log(inputEl);
-    // const hmmm = document.getElementById("id");
-    // var hours = ["09", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-    // var dayHours = {
-    //     id: "ID",
-    //     hour: ["10000"],
-    //     time: "TIME"
-    // };
-    // console.log(dayHours.hour);
-
-    // const hourList = [
-    //     {
-    //         id: "0",
-    //         hour: "09",
-    //         time: "am",
-            
-    //     },
-    //     {
-    //         id: "1",
-    //         hour: "10",
-    //         time: "am",
-            
-
-    //     },
-    //     {
-    //         id: "2",
-    //         hour: "11",
-    //         time: "am",
-            
-    //     },
-    //     {
-    //         Id: "3",
-    //         hour: "12",
-    //         time: "am",
-            
-    //     },
-    //     {
-    //         id: "4",
-    //         hour: "1",
-    //         time: "pm",
-            
-    //     },
-    //     {
-    //         id: "5",
-    //         hour: "2",
-    //         time: "pm",
-            
-    //     },
-    //     {
-    //         id: "6",
-    //         hour: "3",
-    //         time: "pm",
-            
-    //     },
-    //     {
-    //         id: "7",
-    //         hour: "4",
-    //         time: "pm",
-           
-    //     },
-    //     {
-    //         id: "8",
-    //         hour: "5",
-    //         time: "pm",
-            
-    //     },
-    //     {
-    //         id: "9",
-    //         hour: "6",
-    //         time: "pm",
-            
-    //     },
-    //     {
-    //         id: "10",
-    //         hour: "7",
-    //         time: "pm",
-            
-    //     },
-    //     {
-    //         id: "11",
-    //         hour: "8",
-    //         time: "pm",
-            
-    //     },
-    //     {
-    //         id: "12",
-    //         hour: "9",
-    //         time: "pm",
-            
-    //     },
-    // ];
- 
-
-    // for (var i = 0; i < hourList.length; i++) {
-    //     console.log("The Time is " + hourList[i]);
-    // }
-
-       
-    // for (var i = 0; i < dayHours; i++) {
-    //     display.content; newC
-  
-    // }
-
-    // console.log(hourList)
-    // window.localStorage.setItem('time', JSON.stringify(dayHours));
-
-    // //display  (id, hour, time);
-
-
-    // const test = localStorage.getItem('id');
- 
+// $("#8am").data()(localStorage.getItem("8am"));
+$(document).ready(function () {
+  // Here we use jQuery to select the header with "click-me" as its ID.
+  // Notice I have the #click-me, click, and then the function
+  // So $("#id|.class|element").on("action", function(){});
+  // And so whenever it is clicked...
+  $("#saveBtn").on("click", function () {
+    alert("I've been clicked!");
+  });
+});
